86ae4cd9fa36578ec02794a361ab7987e8dde6ec
Victor Avila <victoraguia2009@hotmail.com>
2025-05-22 00:58:17 -0300

    Ajuste al mensaje

--- Js/script.js
+++ Js/script.js
@@ -70,7 +70,7 @@ function getURLParam(name) {
 function showDedicationText() { //seguidores
   let text = getURLParam('text');
   if (!text) {
-    text = `Para mi amor Cinthia:\n\nDesde el primer momento supe que te vi. Tu sonrisa, tu voz, tu forma de ser… todo en ti me hace sentir en casa.\n\nGracias por acompañarme en cada paso, por entenderme incluso en silencio, y por llenar mis días de amor.\n\nTe quiero más de lo que las palabras pueden expresar.`;  } else {
+    text = `Para mi amor Cinthia:\n\nDesde el primer momento que te vi. Tu sonrisa, tu voz, tu forma de ser… todo en ti me hace sentir en casa.\n\nGracias por acompañarme en cada paso, por entenderme incluso en silencio, y por llenar mis días de amor.\n\nTe quiero más de lo que las palabras pueden expresar.`;  } else {
     text = decodeURIComponent(text).replace(/\\n/g, '\n');
   }
   const container = document.getElementById('dedication-text');
